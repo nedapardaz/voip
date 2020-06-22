2f868194ad7093ae7dd2f235e3bdfb0593eee5b9
h.khoshdaman <khoshdaman_20@yahoo.com>
2020-06-22 11:46:08 +0330

    Delete Sample .java

--- src/sample.java
+++
@@ -1,8 +0,0 @@
-public class sample {
-    public static void main(String[] args) {
-        InitializeIVR initializeIVR = new InitializeIVR();
-
-        System.out.println(initializeIVR.ivrConfigurations.getDbURL());
-        System.out.println(initializeIVR.ivrConfigurations.getPassword());
-    }
-}
